d0dcbdb4a7fd20ee3f2d8037f9faa94b32e2f799
ShiyaraSanthosh <shiyarasanthosh3@gmail.com>
2025-06-26 16:00:46 +0530

    palindrome

---
+++ Palindrome Number.js
@@ -0,0 +1,16 @@
+let x = -121;
+var Palindrome = function(x){
+    let str = x.toString();
+    let ispalindrome = "false";
+    for(let i=0; i<str.length; i++){
+        if(str[i]===str[str.length-1-i]){
+            ispalindrome = "True";
+
+        }else{
+            return false;
+        }
+    }
+    return ispalindrome;
+
+}
+console.log(Palindrome(x));
